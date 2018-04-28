05cbe68ae8da49198b73466bee12fd079108adc3
heyongjian <heyongjian@kezaihui.com>
2018-04-28 17:34:27 +0800

    add button animation

--- Calculator/Component/FuncButton.swift
+++ Calculator/Component/FuncButton.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class FuncButton: UIButton {
+    let highlightColor = UIColor(red: 219/255.0, green: 219/255.0, blue: 219/255.0, alpha:1)
 
     /*
     // Only override draw() if you perform custom drawing.
@@ -21,14 +22,30 @@ class FuncButton: UIButton {
     init() {
         super.init(frame: CGRect.zero)
         self.layer.borderWidth = 0.5
-        self.layer.borderColor = UIColor(red: 219/255.0, green: 219/255.0, blue: 219/255.0, alpha:1).cgColor
+        self.layer.borderColor = highlightColor.cgColor
         self.setTitleColor(UIColor.orange, for: UIControlState.normal)
         self.titleLabel?.font = UIFont.systemFont(ofSize: 25)
         self.setTitleColor(UIColor.black, for: UIControlState.highlighted)
+        
+        self.setBackgroundImage(UIImage(color: highlightColor), for: .highlighted)
         
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension UIImage {
+    
+    public convenience init(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
+        let rect = CGRect(origin: CGPoint.zero, size: size)
+        UIGraphicsBeginImageContextWithOptions(size, true, 0)
+        color.setFill()
+        UIRectFill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        self.init(cgImage: (image?.cgImage!)!)
     }
 }
